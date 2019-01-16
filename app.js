f1db6aecec009197ce18ff1f64b932d6ad200f79
Giselle Martel <gmartel90@gmail.com>
2019-01-16 01:07:55 -0500

    fix

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-// external dependencies
-const express = require("express");
-const path = require("path");
-
-//initialize app
+//server.js
+const express = require('express');
 const app = express();
-const port = 8080;
-
-app.use(express.static("./dist/src"));
-
-//set route into app homepage
-app.get("/", (req, res) => {
-    res.sendFile(path.join("./dist/src/index.html"));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'src')));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, 'src/index.html'));
 });
-
-//start server
-app.listen(process.env.PORT || port, () => {
-    console.log("Server started on port " + port);
+app.listen(process.env.PORT || 8080, () => {
+    console.log("listening on port");
 });
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,7 +8,7 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor(private route: Router) {
+  constructor(protected route: Router) {
   }
 
   ngOnInit() {
